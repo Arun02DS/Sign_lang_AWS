f591d79803c5ae662543a656908c2988e873af49
Arun02DS <arun02negime@gmail.com>
2024-01-25 15:36:55 +0530

    pipeline updated

--- SignLanguage/constant/training_pipeline/__init__.py
+++ SignLanguage/constant/training_pipeline/__init__.py
@@ -27,3 +27,6 @@ Model Trainer related constant start with MODEL_TRAINER VAR NAME
 """
 
 MODEL_TRAINER_DIR_NAME:str='model_trainer'
+MODEL_TRAINER_PRETRAINED_WEIGHT_NAME:str="yolov5s.pt"
+MODEL_TRAINER_NO_EPOCHS:int=1
+MODEL_TRAINER_BATCH_SIZE:int=16
